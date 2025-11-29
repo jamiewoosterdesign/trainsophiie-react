0bbe5c3c1c8d13e63edbc0cf7eca8c3028f45edf
jamiewoosterdesign <jamiewoosterdesign@gmail.com>
2025-11-29 23:35:53 +1000

    ok

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,10 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/', // or just remove this line entirely, default is '/'
+    base: '/',
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 })
