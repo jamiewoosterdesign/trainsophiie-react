6eded005dd98d01361b3b9f803c012423f8bdfa5
jamiewoosterdesign <jamiewoosterdesign@gmail.com>
2025-11-29 22:52:23 +1000

    ok

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,20 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command }) => {
+    const config = {
+        plugins: [react()],
+        base: '/',
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+            },
         },
-    },
+    }
+
+    if (command !== 'serve') {
+        config.base = '/trainsophiie-react/'
+    }
+
+    return config
 })
